8c020d400f506ac6241cb37c87e4debb2cc91b04
Mergim <mergimberisha0@gmail.com>
2023-05-02 14:10:08 +0100

    completed first challenge

--- app.rb
+++ app.rb
@@ -22,7 +22,15 @@ class Application < Sinatra::Base
 
   get "/hello" do
     name = params[:name]
+
     return "Hello #{name}"
+  end
+
+  post "/submit" do
+    name = params[:name]
+    message = params[:message]
+
+    return "Thanks #{name}, you sent this message, #{message}"
   end
 end
 
